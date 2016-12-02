01eeb57053bbb30923bc2422b488dfd94641453e
Tang Peng <tchatpeng@gmail.com>
2016-12-02 19:07:30 +0800

    errors

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
-// var bcrypt = require('bcrypt')
-var bcrypt = require('bcrypt-nodejs')
+var bcrypt = require('bcrypt')
+// var bcrypt = require('bcrypt-nodejs')
 
 var userSchema = new mongoose.Schema({
   local: {
